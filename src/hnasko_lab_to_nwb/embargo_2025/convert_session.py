039b0f63771fb9f26d9d4589aba1a4e02bea3973
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-11-20 18:43:45 +0100

    add tdt interface

--- src/hnasko_lab_to_nwb/embargo_2025/convert_session.py
+++ src/hnasko_lab_to_nwb/embargo_2025/convert_session.py
@@ -1,15 +1,20 @@
 """Primary script to run to convert an entire session for of data using the NWBConverter."""
+
 from pathlib import Path
 from typing import Union
-import datetime
-from zoneinfo import ZoneInfo
 
-from neuroconv.utils import load_dict_from_file, dict_deep_update
+from nwbconverter import Embargo2025NWBConverter
 
-from .embargo_2025 import Embargo2025NWBConverter
+from neuroconv.utils import dict_deep_update, load_dict_from_file
 
 
-def session_to_nwb(data_dir_path: Union[str, Path], output_dir_path: Union[str, Path], stub_test: bool = False):
+def session_to_nwb(
+    data_dir_path: Union[str, Path],
+    output_dir_path: Union[str, Path],
+    subject_id: str,
+    session_id: str,
+    stub_test: bool = False,
+):
 
     data_dir_path = Path(data_dir_path)
     output_dir_path = Path(output_dir_path)
@@ -17,37 +22,25 @@ def session_to_nwb(data_dir_path: Union[str, Path], output_dir_path: Union[str, 
         output_dir_path = output_dir_path / "nwb_stub"
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    session_id = "subject_identifier_usually"
-    nwbfile_path = output_dir_path / f"{session_id}.nwb"
+    nwbfile_path = output_dir_path / f"sub-{subject_id}_ses-{session_id}.nwb"
 
     source_data = dict()
     conversion_options = dict()
 
     # Add Recording
-    source_data.update(dict(Recording=dict()))
-    conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
-
-    # Add Sorting
-    source_data.update(dict(Sorting=dict()))
-    conversion_options.update(dict(Sorting=dict()))
-
-    # Add Behavior
-    source_data.update(dict(Behavior=dict()))
-    conversion_options.update(dict(Behavior=dict()))
+    folder_path = data_dir_path / "Fiber photometry_TDT/Varying durations" / f"{subject_id}-{session_id}"
+    source_data.update(dict(FiberPhotometry=dict(folder_path=folder_path)))
+    conversion_options.update(dict(FiberPhotometry=dict()))
 
     converter = Embargo2025NWBConverter(source_data=source_data)
 
-    # Add datetime to conversion
-    metadata = converter.get_metadata()
-    date = datetime.datetime(year=2020, month=1, day=1, tzinfo=ZoneInfo("US/Eastern"))
-    metadata["NWBFile"]["session_start_time"] = date
-
     # Update default metadata with the editable in the corresponding yaml file
-    editable_metadata_path = Path(__file__).parent / "embargo_2025_metadata.yaml"
+    metadata = converter.get_metadata()
+    editable_metadata_path = Path(__file__).parent / "metadata.yaml"
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(metadata, editable_metadata)
 
-    metadata["Subject"]["subject_id"] = "a_subject_id"  # Modify here or in the yaml file
+    metadata["Subject"]["subject_id"] = subject_id  # Modify here or in the yaml file
 
     # Run conversion
     converter.run_conversion(metadata=metadata, nwbfile_path=nwbfile_path, conversion_options=conversion_options)
@@ -56,15 +49,15 @@ def session_to_nwb(data_dir_path: Union[str, Path], output_dir_path: Union[str, 
 if __name__ == "__main__":
 
     # Parameters for conversion
-    data_dir_path = Path("/Directory/With/Raw/Formats/")
-    output_dir_path = Path("~/conversion_nwb/")
+    data_dir_path = Path("D:/Hnasko-CN-data-share/SN pan GABA recordings/PPN/")
+    output_dir_path = Path("D:/hnasko_lab_conversion_nwb")
     stub_test = False
-
-<<<<<<< HEAD:hnasko-lab-to-nwb/src/hnasko_lab_to_nwb/embargo_2025/convert_session.py
-    session_to_nwb(data_dir_path=data_dir_path, output_dir_path=output_dir_path, stub_test=stub_test)
-=======
-    session_to_nwb(data_dir_path=data_dir_path,
-                    output_dir_path=output_dir_path,
-                    stub_test=stub_test,
-                    )
->>>>>>> main:hnasko-lab-to-nwb/src/hnasko_lab_to_nwb/embargo_2025/embargo_2025_convert_session.py
+    subject_id = "C4550"
+    session_id = "240118-094128"
+    session_to_nwb(
+        data_dir_path=data_dir_path,
+        output_dir_path=output_dir_path,
+        subject_id=subject_id,
+        session_id=session_id,
+        stub_test=stub_test,
+    )
