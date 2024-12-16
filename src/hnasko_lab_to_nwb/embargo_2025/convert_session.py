e7cf001d27c26deb0a88aedf80682e1ae730ef99
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-12-16 18:55:45 +0100

    update conversion script

--- src/hnasko_lab_to_nwb/embargo_2025/convert_session.py
+++ src/hnasko_lab_to_nwb/embargo_2025/convert_session.py
@@ -9,14 +9,13 @@ from neuroconv.utils import dict_deep_update, load_dict_from_file
 
 
 def session_to_nwb(
-    data_dir_path: Union[str, Path],
     output_dir_path: Union[str, Path],
     subject_id: str,
     session_id: str,
+    tdt_folder_path: Union[str, Path],
+    protocol_type: str,
     stub_test: bool = False,
 ):
-
-    data_dir_path = Path(data_dir_path)
     output_dir_path = Path(output_dir_path)
     if stub_test:
         output_dir_path = output_dir_path / "nwb_stub"
@@ -24,12 +23,34 @@ def session_to_nwb(
 
     nwbfile_path = output_dir_path / f"sub-{subject_id}_ses-{session_id}.nwb"
 
+    valid_protocols = {"Varying durations", "Varying frequencies", "Shocks"}
+    if protocol_type not in valid_protocols:
+        raise ValueError(f"Invalid protocol_type: {protocol_type}. Must be one of {valid_protocols}.")
+    if protocol_type == "Varying durations":
+        session_description = (
+            "The subject is placed in a plastic tub and is recorded for 3.5 minutes. "
+            "The subject receives a 40 Hz stimulation at various durations (i.e. 250ms, 1s and 4s) "
+            "5 times for each duration) with an inter-stimulus interval (ISI) of 10s. "
+        )
+    elif protocol_type == "Varying frequencies":
+        session_description = (
+            "The subject is placed in a plastic tub and undergoes 3 recording sessions corresponding "
+            "to a fixed duration of stimulation (i.e., 250ms, 1s, and 4s). Each session lasted 8 minutes. "
+            "The subject receives optogenetic stimulation at varying frequencies "
+            "(5 Hz, 10 Hz , 20 Hz and 40 Hz) 5 times for each duration with an ISI of 10s. "
+        )
+    elif protocol_type == "Shocks":
+        session_description = (
+            "The subject is placed in a shock chamber and recorded for 6 minutes. "
+            "Uncued shocks (0.3 mA) at various durations (250ms, 1s and 4s, 5 times for each duration) "
+            "are delivered in a randomized order and ISI."
+        )
     source_data = dict()
     conversion_options = dict()
 
-    # Add Recording
-    folder_path = data_dir_path / "Fiber photometry_TDT/Varying durations" / f"{subject_id}-{session_id}"
-    source_data.update(dict(FiberPhotometry=dict(folder_path=folder_path)))
+    # Add FiberPhotometry
+
+    source_data.update(dict(FiberPhotometry=dict(folder_path=tdt_folder_path)))
     conversion_options.update(dict(FiberPhotometry=dict()))
 
     converter = Embargo2025NWBConverter(source_data=source_data)
@@ -40,10 +61,14 @@ def session_to_nwb(
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(metadata, editable_metadata)
 
-    metadata["Subject"]["subject_id"] = subject_id  # Modify here or in the yaml file
+    metadata["Subject"]["subject_id"] = subject_id
+    metadata["NWBFile"]["session_id"] = session_id
+    metadata["NWBFile"]["session_description"] = session_description
 
     # Run conversion
-    converter.run_conversion(metadata=metadata, nwbfile_path=nwbfile_path, conversion_options=conversion_options)
+    converter.run_conversion(
+        metadata=metadata, nwbfile_path=nwbfile_path, conversion_options=conversion_options, overwrite=True
+    )
 
 
 if __name__ == "__main__":
@@ -51,13 +76,26 @@ if __name__ == "__main__":
     # Parameters for conversion
     data_dir_path = Path("D:/Hnasko-CN-data-share/SN pan GABA recordings/PPN/")
     output_dir_path = Path("D:/hnasko_lab_conversion_nwb")
-    stub_test = False
-    subject_id = "C4550"
-    session_id = "240118-094128"
-    session_to_nwb(
-        data_dir_path=data_dir_path,
-        output_dir_path=output_dir_path,
-        subject_id=subject_id,
-        session_id=session_id,
-        stub_test=stub_test,
-    )
+    from neuroconv.tools.path_expansion import LocalPathExpander
+
+    data_dir_path = "D:/Hnasko-CN-data-share/SN pan GABA recordings/PPN/"
+    # Specify source data
+    source_data_spec = {
+        "FiberPhotometry": {
+            "base_directory": data_dir_path + "Fiber photometry_TDT",
+            "folder_path": "{protocol_type}/{subject_id}-{session_id}",
+        }
+    }
+    # Instantiate LocalPathExpander
+    path_expander = LocalPathExpander()
+    # Expand paths and extract metadata
+    metadata_list = path_expander.expand_paths(source_data_spec)
+    for metadata in metadata_list:
+        session_to_nwb(
+            output_dir_path=output_dir_path,
+            subject_id=metadata["metadata"]["Subject"]["subject_id"],
+            session_id=metadata["metadata"]["NWBFile"]["session_id"],
+            tdt_folder_path=metadata["source_data"]["FiberPhotometry"]["folder_path"],
+            protocol_type=metadata["metadata"]["extras"]["protocol_type"],
+            stub_test=True,
+        )
