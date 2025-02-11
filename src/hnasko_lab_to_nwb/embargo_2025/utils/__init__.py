341bfb1007ebdb3047ec2d2b2e51337a37556503
alessandratrapani <alessandramaria.trapani@gmail.com>
2025-02-11 15:21:56 +0100

    add support for shock stimuli in NWB conversion process

--- src/hnasko_lab_to_nwb/embargo_2025/utils/__init__.py
+++ src/hnasko_lab_to_nwb/embargo_2025/utils/__init__.py
@@ -1,5 +1,4 @@
-from src.hnasko_lab_to_nwb.embargo_2025.utils.add_optogenetic_stimulation import (
-    add_optogenetic_stimulation,
-)
+from .add_optogenetic_stimulation import add_optogenetic_stimulation
+from .add_shock_stimuli import add_shock_stimuli
 
-__all__ = ["add_optogenetic_stimulation"]
+__all__ = ["add_optogenetic_stimulation", "add_shock_stimuli"]
