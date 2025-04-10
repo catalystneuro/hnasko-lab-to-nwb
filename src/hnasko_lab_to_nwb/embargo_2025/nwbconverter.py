e4bfdd8bee2e4ed4940771bc50efae908b9110b4
Daphne de Quatrebarbes <daphne.dequatrebarbes@epfl.ch>
2025-04-10 20:28:58 +0200

    feat: add FiberPhotometryResponseSeries support to NWB converter

--- src/hnasko_lab_to_nwb/embargo_2025/nwbconverter.py
+++ src/hnasko_lab_to_nwb/embargo_2025/nwbconverter.py
@@ -2,7 +2,10 @@
 
 from typing import Optional
 
+import pynwb
 from pynwb import NWBFile
+
+from utils.add_fiber_photometry_response_series import add_fiber_photometry_response_series
 from utils import add_auditory_stimuli, add_optogenetic_stimulation, add_shock_stimuli
 
 from neuroconv import NWBConverter
@@ -21,6 +24,12 @@ class Embargo2025NWBConverter(NWBConverter):
         if "FiberPhotometry" in self.data_interface_objects.keys():
             tdt_interface = self.data_interface_objects["FiberPhotometry"]
             tdt_events = tdt_interface.get_events()
+            
+            # Add FiberPhotometryResponseSeries
+            if "FiberPhotometryResponseSeries" in metadata["Ophys"]["FiberPhotometry"]:
+                add_fiber_photometry_response_series(nwbfile=nwbfile,metadata=metadata["Ophys"]["FiberPhotometry"], tdt_interface=tdt_interface,
+                )
+
             if "OptogeneticStimulusInterval" in metadata["Stimulus"]:
                 add_optogenetic_stimulation(nwbfile=nwbfile, metadata=metadata, tdt_events=tdt_events)
             if "ShockStimulusInterval" in metadata["Stimulus"]:
